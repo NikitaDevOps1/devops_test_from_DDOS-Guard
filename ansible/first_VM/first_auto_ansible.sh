ca1778ae65d44b6d2de2280376d162023deb475f
Nikita Medvedev <nikita.medvedevsa23@gmail.com>
2024-10-27 15:11:21 +0300

    hidden the pass

--- ansible/first_VM/first_auto_ansible.sh
+++ ansible/first_VM/first_auto_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook first_setting.yml -e "ansible_user=user ansible_password=1 ansible_become_method=su ansible_become_user=root ansible_become_password=1" -vv
+ansible-playbook first_setting.yml -e "ansible_user=user ansible_password=pass ansible_become_method=su ansible_become_user=root ansible_become_password=pass" -vv
--- ansible/second_VM/second_auto_ansible.sh
+++ ansible/second_VM/second_auto_ansible.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook second_setting.yml -e "ansible_user=user ansible_password=1 ansible_become_method=su ansible_become_user=root ansible_become_password=1" -vvvv
+ansible-playbook second_setting.yml -e "ansible_user=user ansible_password=pass ansible_become_method=su ansible_become_user=root ansible_become_password=pass" 
